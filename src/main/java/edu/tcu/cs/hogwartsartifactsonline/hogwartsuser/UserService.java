44d7734f6c2217aa80103aa424fe80fd4e909f9a
Paige Anderson <panderson.pdx@gmail.com>
2023-03-22 21:30:55 -0500

    User CRUD done! Close #5

---
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserService.java
@@ -0,0 +1,47 @@
+package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser;
+
+import edu.tcu.cs.hogwartsartifactsonline.system.exception.ObjectNotFoundException;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<HogwartsUser> findAll(){
+        return this.userRepository.findAll();
+    }
+
+    public HogwartsUser findById(Integer userId){
+        return this.userRepository.findById(userId)
+                .orElseThrow(() -> new ObjectNotFoundException("user", userId));
+    }
+
+    public HogwartsUser save(HogwartsUser newHogwartsUser){
+        // We NEED to encode plain password before saving to the DB
+        return this.userRepository.save(newHogwartsUser);
+    }
+
+    public HogwartsUser update(Integer userId, HogwartsUser update) {
+        HogwartsUser oldHogwartsUser = this.userRepository.findById(userId)
+                .orElseThrow(() -> new ObjectNotFoundException("user", userId));
+        oldHogwartsUser.setUsername(update.getUsername());
+        oldHogwartsUser.setEnabled(update.isEnabled());
+        oldHogwartsUser.setRole(update.getRole());
+        return this.userRepository.save(oldHogwartsUser);
+    }
+
+    public void delete(Integer userId){
+        this.userRepository.findById(userId)
+                .orElseThrow(()-> new ObjectNotFoundException("user", userId));
+        this.userRepository.deleteById(userId);
+    }
+}
