8e51fdc0c6bd02ddb920b7a0339a70bb40d22e75
Paige Anderson <panderson.pdx@gmail.com>
2023-04-24 00:54:15 -0500

    all the code but still issues with soem of the security files

--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserService.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/hogwartsuser/UserService.java
@@ -2,18 +2,25 @@ package edu.tcu.cs.hogwartsartifactsonline.hogwartsuser;
 
 import edu.tcu.cs.hogwartsartifactsonline.system.exception.ObjectNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 @Transactional
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
+    private PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
     public List<HogwartsUser> findAll(){
@@ -27,6 +34,7 @@ public class UserService {
 
     public HogwartsUser save(HogwartsUser newHogwartsUser){
         // We NEED to encode plain password before saving to the DB
+        newHogwartsUser.setPassword(this.passwordEncoder.encode(newHogwartsUser.getPassword()));
         return this.userRepository.save(newHogwartsUser);
     }
 
@@ -43,5 +51,12 @@ public class UserService {
         this.userRepository.findById(userId)
                 .orElseThrow(()-> new ObjectNotFoundException("user", userId));
         this.userRepository.deleteById(userId);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return this.userRepository.findByUsername(username) // first find user from database
+                .map(hogwartsUser -> new MyUserPrincipal(hogwartsUser)) // if found, wrap the user instance in a myuserprincipal instance
+                .orElseThrow(() -> new UsernameNotFoundException("username " + username + " is not found.")); // otherwise throw an exception
     }
 }
--- src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
+++ src/main/java/edu/tcu/cs/hogwartsartifactsonline/system/exception/ExceptionHandlerAdvice.java
@@ -5,6 +5,11 @@ import edu.tcu.cs.hogwartsartifactsonline.system.Result;
 import edu.tcu.cs.hogwartsartifactsonline.system.StatusCode;
 import edu.tcu.cs.hogwartsartifactsonline.wizard.WizardNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.authentication.AccountStatusException;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.oauth2.server.resource.InvalidBearerTokenException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -47,5 +52,39 @@ public class ExceptionHandlerAdvice {
             map.put(key, val);
         });
         return new Result(false, StatusCode.INVALID_ARGUMENT, "Provided arguments are invalid, see data for details.", map);
+    }
+
+    @ExceptionHandler({UsernameNotFoundException.class, BadCredentialsException.class})
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    Result handleAuthenticationException(Exception ex){
+        return new Result(false, StatusCode.UNAUTHORIZED,"username or password is incorrect", ex.getMessage());
+    }
+    @ExceptionHandler(AccountStatusException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    Result handleAccountStatusException(AccountStatusException ex){
+        return new Result(false, StatusCode.UNAUTHORIZED,"User account is abnormal", ex.getMessage());
+    }
+
+    @ExceptionHandler(InvalidBearerTokenException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    Result handleInvalidBearerTokenException(InvalidBearerTokenException ex){
+        return new Result(false, StatusCode.UNAUTHORIZED,"The access token provided is expiered, revoked, malformed, or invalid for other reasons", ex.getMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    Result handleAccessDeniedException(AccessDeniedException ex){
+        return new Result(false, StatusCode.FORBIDDEN,"No permission", ex.getMessage());
+    }
+
+    /**
+     * Fallback handles any unhandled exceptions
+     * @param ex
+     * @return
+     */
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    Result handleOtherException(Exception ex){
+        return new Result(false, StatusCode.INTERNAL_SERVER_ERROR,"A server internal error occurs", ex.getMessage());
     }
 }
